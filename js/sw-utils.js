66f74f20e70a538a4fd5871a78bb61f746c65beb
Miguel Muñoz Medina <miguel.angel.medina.1994@gmail.com>
2019-01-30 17:17:49 -0600

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,22 @@
+
+//Guardar el cache dynamic
+function actualizaCacheDynamico( dynamicCache, req, res ){
+
+    if(res.ok){
+
+        return caches.open(dynamicCache).then(cache => {
+
+            cache.put( req, res.clone() )
+
+            return res.clone()
+
+        })
+
+    }else{
+
+        return res;
+
+    }
+
+}
+
